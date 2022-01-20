d58213aa89c993db876966bbc25617673b660fba
minna <minna.nguyen@tuni.fi>
2022-01-20 15:07:20 +0200

    hifistely e17, tehtävät 18 - 19 tehty

--- homework/01/e17.py
+++ homework/01/e17.py
@@ -1,13 +1,13 @@
 import random
-
+# arvotaan satunnaisluku 0 - 10 väliltä
 salainen_luku = random.randint(0, 10)
 kayttajan_syote = -1
 
+laskuri = 0
 while kayttajan_syote != salainen_luku:
     print("Arvaa salainen luku (0 - 10)")
     kayttajan_syote = int( input() )
+    laskuri += 1
 
-print("oikein!")
-
-
-
+print("Oikein!", "Arvasit näin monta kertaa:")
+print(laskuri)
