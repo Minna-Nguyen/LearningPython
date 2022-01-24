f4e45088c6756420b526a5f9809e6493a8cbf8e1
minna <minna.nguyen@tuni.fi>
2022-01-24 21:26:04 +0200

    tehtävä 21

---
+++ homework/01/e21.py
@@ -0,0 +1,17 @@
+print("Anna suorakaiteen korkeus")
+korkeus = int(input())
+print("Anna suorakaiteen leveys")
+leveys = int(input())
+MääräKorkeus = 0 #rivi
+MääräLeveys = 0 #sarake
+
+while MääräKorkeus < korkeus:
+  while MääräLeveys < leveys:
+    #Tulosta "X" ilman enter painallusta.
+    print("X", end = '')
+    MääräLeveys += 1
+  #Nollataan MääräLeveys jotta päästän takaisin loopin sisään (rivi 9).
+  MääräLeveys = 0
+  #Tulosta enter
+  print()
+  MääräKorkeus += 1
