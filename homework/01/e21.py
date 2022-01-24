31ad8d9c76b355f9788f76addf9bb13effc7137c
minna <minna.nguyen@tuni.fi>
2022-01-24 21:29:37 +0200

    pieniä muokauksia kommenteissa e21

--- homework/01/e21.py
+++ homework/01/e21.py
@@ -2,16 +2,16 @@ print("Anna suorakaiteen korkeus")
 korkeus = int(input())
 print("Anna suorakaiteen leveys")
 leveys = int(input())
-MääräKorkeus = 0 #rivi
-MääräLeveys = 0 #sarake
+MääräKorkeus = 0 # rivi
+MääräLeveys = 0 # sarake
 
 while MääräKorkeus < korkeus:
   while MääräLeveys < leveys:
-    #Tulosta "X" ilman enter painallusta.
+    # Tulosta "X" ilman enter painallusta.
     print("X", end = '')
     MääräLeveys += 1
-  #Nollataan MääräLeveys jotta päästän takaisin loopin sisään (rivi 9).
+  # Nollataan MääräLeveys jotta päästän takaisin loopin sisään (rivi 9).
   MääräLeveys = 0
-  #Tulosta enter
+  # Tulosta enter
   print()
   MääräKorkeus += 1
