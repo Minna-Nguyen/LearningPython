22de4f5ec7495c70aae7b0f01945470809b0bc64
minna <minna.nguyen@tuni.fi>
2022-01-25 10:52:21 +0200

    tehtävä 22 tehty

---
+++ homework/01/e22.py
@@ -0,0 +1,14 @@
+print("Anna jokin luku")
+luku = int(input())
+# tulostetaan tyhjä väli
+print()
+
+laskuri = 0
+if luku > laskuri:
+    while luku >= laskuri:
+        print(luku)
+        luku -= 1
+else:
+    while luku <= laskuri:
+        print(luku)
+        luku += 1
