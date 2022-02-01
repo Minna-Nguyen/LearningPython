62b51412c110b04ba424798d2cb19595891de599
minna <minna.nguyen@tuni.fi>
2022-02-01 10:24:27 +0200

    fixed camelCase to snake_case

--- homework/02/e08.py
+++ homework/02/e08.py
@@ -1,13 +1,11 @@
 # function that returns an int between range a - b
 def get_int(sentence, a, b):
-    grade = int(input(sentence))
-    while grade < a or grade > b:
-        print("Grade must be 4 - 10")
-        grade = int(input())
-    return grade
+    age = int(input(sentence))
+    while age < a or age > b:
+        print("Age must be between 0 - 120")
+        age = int(input())
+    return age
 # calling the function
-grade = get_int("Give grade:", 4, 10,)
-print("You gave:", grade)
+age = get_int("Give age: ", 0, 120,)
+print("You gave:", age)
     
-
-
