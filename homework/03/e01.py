b6b988de29ece547764c030fb37acb734ee55dd1
minna <minna.nguyen@tuni.fi>
2022-02-07 17:18:33 +0200

    modified e01.oy

--- homework/03/e01.py
+++ homework/03/e01.py
@@ -1,1 +1,25 @@
-print("This is exercise 01")
+# printing 1 to 10
+for index in range(1,11):
+    # last index of range
+    if index == 10:
+        print(index, end = "")
+    else:
+        print(index, end = ",")
+print()
+# printing every second index from 0 to 10
+for index in range(0,12, 2):
+    # last index of range
+    if index == 10:
+        print(index, end = "")
+    else:
+        print(index, end = ",")
+
+print()
+# # print 10 to 0
+for index in range(10,-1, -1):
+    # last index of range
+    if index == 0:
+        print(index, end = "")
+    else:
+        print(index, end = ",")
+print()
