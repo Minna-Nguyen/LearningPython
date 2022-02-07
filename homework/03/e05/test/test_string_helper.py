e094236f4123031a21f22b8ab1766e229c835cff
minna <minna.nguyen@tuni.fi>
2022-02-07 17:20:17 +0200

    e05 done

---
+++ homework/03/e05/test/test_string_helper.py
@@ -0,0 +1,22 @@
+import unittest
+
+from string_helper import get_title
+
+class TestStringHelper(unittest.TestCase):
+    def test_get_title(self):
+        
+        test_title1 = """
+------------------------------
+-         Battleship         -
+------------------------------
+            """.strip() # strip() will remove extra enters (\n} from start and end.
+
+        self.assertEqual(get_title("battleship", 30, "-"), test_title1)
+       
+        test_title2 = """
+****************************************
+*            Palindrome App            *
+****************************************
+            """.strip()
+
+        self.assertEqual(get_title("palindrome app", 40, "*"), test_title2)
