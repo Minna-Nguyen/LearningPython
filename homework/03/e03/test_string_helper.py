21fb6ed4c9764cead803a3a911ff4a6ae1cec6b9
minna <minna.nguyen@tuni.fi>
2022-02-09 14:10:23 +0200

    added more tests

--- homework/03/e03/test_string_helper.py
+++ homework/03/e03/test_string_helper.py
@@ -5,17 +5,40 @@ from string_helper import is_name
 class TestStringHelper(unittest.TestCase):
     def test_is_name(self):
         # with assertEqual you can check if two values the same:
-        self.assertEqual(is_name("Ville Virtanen"), True)
-
-        # but if the function just returns True or False, it may be easier
+          # but if the function just returns True or False, it may be easier
         # to use assertTrue of assertFalse:
         self.assertTrue(is_name("Ville Virtanen"))
-        self.assertFalse(is_name("ville virtanen"))
+        self.assertFalse(is_name("testi nimi"))
+        self.assertFalse(is_name("testi Nimi"))
+        self.assertFalse(is_name("Testi nimi"))
+        self.assertFalse(is_name("12 nimi"))
+        self.assertFalse(is_name("123 123?="))
+        self.assertFalse(is_name("VilleVirtanen"))
+
         self.assertFalse(is_name("12Villa"))
         self.assertFalse(is_name("ville"))
-
+        self.assertFalse(is_name("Honney"))
+        self.assertFalse(is_name(" ville"))
+        self.assertFalse(is_name(" Melissa"))
+        self.assertFalse(is_name("mille "))
+        # self.assertFalse(is_name(" Viii Mooi "))
+        # self.assertFalse(is_name(" Viii Mooi"))
+        # self.assertFalse(is_name("Viii Mooi "))
+        self.assertFalse(is_name("?? Hello"))
         # task number 3:
         self.assertTrue(is_name("ville virtanen", ignore_case=True))
         self.assertFalse(is_name("ville virtanen", ignore_case=False))
         self.assertTrue(is_name("Ville Virtanen", ignore_case=False))
+        self.assertFalse(is_name("123 virtanen", ignore_case=False))
+        self.assertFalse(is_name("ville 12!dew", ignore_case=False))
+        self.assertFalse(is_name("123 virtanen", ignore_case=True))
+        self.assertFalse(is_name("ville 12!dew", ignore_case=True))
+        self.assertFalse(is_name("VilleVirtanen", ignore_case=True))
+        self.assertFalse(is_name("12Villa", ignore_case=True))
+        self.assertFalse(is_name("ville", ignore_case=True))
+        self.assertFalse(is_name("Honney", ignore_case=True))
         
+        
+        self.assertFalse(is_name("12Villa", ignore_case=False))
+        self.assertFalse(is_name("ville", ignore_case=False))
+        self.assertFalse(is_name("Honney", ignore_case=False))
