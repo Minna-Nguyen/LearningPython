7c0307db7f9fa86ee6ec269cdcf2d429cb59b61b
minna <minna.nguyen@tuni.fi>
2022-02-09 11:42:40 +0200

    04/e02: complete

--- homework/04/e01/main.py
+++ homework/04/e01/main.py
@@ -1,10 +1,15 @@
 from random import randint
 
+def generate_lotto_numbers(a,b):
 # A set is a collection which is unordered, unchangeable, and no duplicates
-lotto_numbers = set()
+    lotto_numbers = set()
+    if type(a) != int or type(b) != int:
+        raise Exception("Must be an int type!")
+    else:
+    # this will print 7 random number elements that are not duplicate (unique numbers)
+        while len(lotto_numbers) < 7:
+            lotto_numbers.add(randint(a,b))
 
-# this will print 7 random number elements that are not duplicate (unique numbers)
-while len(lotto_numbers) < 7:
-    lotto_numbers.add(randint(1,40))
+    return lotto_numbers
 
-print(lotto_numbers)
+print(generate_lotto_numbers(1,40))
