5bbbd0f18cf0a29bacbaead7b187298cd62cc69c
minna <minna.nguyen@tuni.fi>
2022-02-22 13:31:35 +0200

    implementing the user_input functions

---
+++ homework/05/main.py
@@ -0,0 +1,29 @@
+from util.user_input import *
+from util.validation import *
+
+def main():
+    menu = {1: "Add", 0: "Exit"}
+
+    employees = []
+
+    continue_loop = True
+    while continue_loop:
+        print()
+        # display menu options
+        print("Menu:")
+        ask(menu=menu)
+        print()
+        # asking about the menu options action
+        user_input = ask_int(input("Your choice: "), 0, len(menu)-1)
+        print()
+
+        # add new name
+        if user_input == 0:
+            employee = ask_person()
+            print()
+            employees.append(employee)
+            print(employees)
+        else:
+            continue_loop = False
+
+main()
