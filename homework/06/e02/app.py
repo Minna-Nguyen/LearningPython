eee7b2b6cd5e897685be2dadcd9d4e6f7bbe84b4
minna <minna.nguyen@tuni.fi>
2022-02-23 10:58:49 +0200

    complete

---
+++ homework/06/e02/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import datetime
+# create Flask object, give module name
+# where to look for resources, like templates or static files
+app = Flask(__name__)
+
+# if url is in root
+@app.route("/hello")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/myname")
+def myname():
+    return "<p>Minna<p/>"
+
+@app.route("/date")
+def date():
+    x = datetime.datetime.now()
+    return f"{x}"
+
+# start the app if using python3 app.py
+if __name__ == "__main__":
+    app.run(debug=True)
