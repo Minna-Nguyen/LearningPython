c196e8bc7bba18531c479341c8c7e12c4e2eecaf
Minna Nguyen <minna.nguyen@tuni.fi>
2022-03-26 19:56:34 +0200

    fetch functions and delete functions OK

---
+++ homework/09/main.py
@@ -0,0 +1,62 @@
+from flask import Flask, jsonify, request, make_response
+import json
+
+
+app = Flask(__name__)
+
+id = 3
+blogs = [{"id": 1, "title": "titleone", "body": "bunch of texts"}, {"id": 2, "title": "title two", "body": "bunch of texts"}, {
+    "id": 3, "title": "sunt aut facere repellat provident - optio reprehenderit", "body": "quia et suscipit\nsuscipit recusa... "}]
+
+# listaa kaikki blogit
+@app.route('/blogs')
+def get_blogs():
+    return jsonify(blogs)
+
+# lisää uusi blogi 
+@app.route('/blogs', methods=['POST'])
+def add_blog():
+    blog = json.loads(request.data)
+    # https://www.programiz.com/python-programming/global-keyword
+    global id
+    id = id + 1
+    blog["id"] = id
+    blogs.append(blog)
+    return make_response(jsonify(blog), 201)
+
+@app.route('/blogs/<int:id>', methods=['GET'])
+def get_certain_blog(id):
+    for i in range(0, len(blogs)):
+        if  blogs[i]["id"] == id:
+            result = jsonify(blogs[i])
+            return result
+
+
+@app.route('/blogs/<int:the_id>', methods=['DELETE'])
+def delete_id(the_id):
+    index_to_be_deleted = -1
+    for i in range(0, len(blogs)):
+        if(blogs[i]["id"] == the_id):
+            index_to_be_deleted = i
+    if(index_to_be_deleted != -1):
+        blogs.pop(index_to_be_deleted)
+        return make_response("", 204)
+    else:
+        return make_response("", 404)
+
+@app.route('/blogs/delete_all', methods=['DELETE'])
+def delete_blog():
+    for i in range(0, len(blogs)):
+        if blogs[i] != None:
+            blogs.clear()
+            if blogs == []:
+                return make_response("", 204)
+            else:
+                return make_response("", 404)
+
+        if request.method == "POST":
+            return "hello"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
