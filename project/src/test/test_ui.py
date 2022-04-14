528f20c9cbf4a80ab1895bf744a29b61d6ff04f0
Minna Nguyen <minna.nguyen@tuni.fi>
2022-04-14 23:22:42 +0300

    add unittesting for functions validate_player_input() and ask_name()

--- project/src/test/test_ui.py
+++ project/src/test/test_ui.py
@@ -1,12 +1,28 @@
 import unittest
-
-import ui
-
+from ui import validate_player_input
+from ui import ask_name
 
 # python3 -m unittest test.test_ui
 
 
-class TestUi(unittest.TestCase):
+class Test_letter(unittest.TestCase):
 
-    def test_get_title(self):
-        self.assertEqual(ui.get_title(), "Battleship")
+    def test_validate_player_input(self):
+        self.assertTrue(validate_player_input("l"))
+
+        self.assertFalse(validate_player_input("lasd"))
+        self.assertFalse(validate_player_input("123"))
+        self.assertFalse(validate_player_input("1"))
+
+    def test_ask_name(self):
+        self.assertTrue(ask_name("Helena"))
+        self.assertTrue(ask_name("Ty"))
+
+        self.assertFalse(ask_name("123"))
+        self.assertFalse(ask_name("1"))
+        self.assertFalse(ask_name("helena"))
+        self.assertFalse(ask_name("H B"))
+        self.assertFalse(ask_name("öä1p23"))
+        self.assertFalse(ask_name(" he  lena"))
+        self.assertFalse(ask_name("a"))
+        self.assertFalse(ask_name("A"))
